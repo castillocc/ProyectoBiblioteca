b9f3176fd0da74b970b6775666d1b15aebad313d
Cristian Castillo <cristian.castillo.campos1993@gmail.com>
2018-02-16 08:24:06 -0600

    Update ILibroServicio.cs

--- BibliotecaApi/Biblioteca.Service/ILibroServicio.cs
+++ BibliotecaApi/Biblioteca.Service/ILibroServicio.cs
@@ -1,1 +1,17 @@
+using Biblioteca.Data.Models;
+using Biblioteca.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
 
+namespace Biblioteca.Service
+{
+    public interface ILibroServicio
+    {
+        IEnumerable<Libro> ListarLibros);
+        Libro ObtenerLibro(int id);
+        void InsertarLibro(Libro libro);
+        void ActualizarLibroes(int id,Libro libro);
+        void EliminarLibroes(int id); 
+    }
+}
