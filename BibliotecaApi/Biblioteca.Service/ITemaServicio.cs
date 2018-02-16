c355ec0b7524c5dd4e057c33e9ba61a287fefd1a
Cristian Castillo <cristian.castillo.campos1993@gmail.com>
2018-02-16 08:53:21 -0600

    Create ITemaServicio.cs

---
+++ BibliotecaApi/Biblioteca.Service/ITemaServicio.cs
@@ -0,0 +1,17 @@
+using Biblioteca.Data.Models;
+using Biblioteca.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Biblioteca.Service
+{
+    public interface ITemaServicio
+    {
+        IEnumerable<Tema> ListarTemas);
+        Tema ObtenerTema(int id);
+        void InsertarTema(Tema tema);
+        void ActualizarTema(int id,Tema tema);
+        void EliminarTema(int id); 
+    }
+}
